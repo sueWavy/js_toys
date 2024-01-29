9d1433bb8450af9e56384fa6a1c77f367b8bb0f9
sueWavy <sueboi0311@gmail.com>
2024-01-29 11:30:11 +0900

    무한 스크롤

--- case6_infinite_scroll/practice/script.js
+++ case6_infinite_scroll/practice/script.js
@@ -4,4 +4,81 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  let page = 1
+  const limit = 10
+  const $posts = get('.posts')
+  const end = 100
+  let total = 10
+
+  const $loader = get('.loader')
+
+  const getPost = async () => {
+    const API_URL = `https://jsonplaceholder.typicode.com/posts?_page=${page}&limit=${limit}`
+    const res = await fetch(API_URL)
+    if (!res.ok) {
+      throw new Error('error')
+    }
+    return await res.json()
+  }
+
+  const showPosts = (posts) => {
+    posts.forEach((post) => {
+      const $post = document.createElement('div')
+      $post.classList.add('post')
+      $post.innerHTML = `
+      <div class="header">
+        <div class="id">${post.id}</div>
+        <div class="title">${posts.title}</div>
+      </div>
+      <div class="body">
+      ${post.body}
+      </div>
+      `
+      $posts.appendChild($post)
+    })
+  }
+
+  const showLoader = () => {
+    $loader.classList.add('show')
+  }
+
+  const hideLoader = () => {
+    $loader.classList.remove('show')
+  }
+
+  const loadPost = async () => {
+    // 로더 보이게하기
+    showLoader()
+
+    try {
+      const res = await getPost()
+      showPosts(res)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      
+      // 로더 안보이게하기
+      hideLoader()
+    }
+  }
+
+  const onScroll = () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement
+
+    if (total === end) {
+      window.removeEventListener('scroll', onScroll)
+      return
+    }
+    if (scrollTop + clientHeight >= scrollHeight - 5) {
+      page++
+      total += 10
+      loadPost()
+    }
+  }
+
+  window.addEventListener('DOMContentLoaded', () => {
+    loadPost()
+    window.addEventListener('scroll', onScroll)
+  })
 })()
