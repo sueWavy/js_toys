26d863b6382de7882c6e5ec159e8b440e16e3064
sueWavy <sueboi0311@gmail.com>
2024-01-29 10:42:36 +0900

    드럼 킷 만들기

--- case4_drumkit/practice/script.js
+++ case4_drumkit/practice/script.js
@@ -9,6 +9,7 @@
     return document.querySelectorAll(target)
   }
 
+  const keys = Array.from(getAll('.key'))
   const soundsRoot = 'assets/sounds/'
   const drumSounds = [
     { key: 81, sound: 'clap.wav' },
@@ -21,4 +22,50 @@
     { key: 88, sound: 'snare.wav' },
     { key: 67, sound: 'tom.wav' },
   ]
+
+  const getAudioElement = (index) => {
+    const audio = document.createElement('audio')
+    audio.dataset.key = drumSounds[index].key
+    audio.src = soundsRoot + drumSounds[index].sound
+    return audio
+  }
+
+  const onTransitionEnd = (e) => {
+    if (e.propertyName === 'transform') {
+      e.target.classList.remove('playing')
+    }
+  }
+
+  const onMouseDown = (e) => {
+    const keycode = e.target.getAttribute('data-key')
+    playSound(keycode)
+  }
+
+  const onKeyDown = (e) => {
+    const keycode = e.keyCode
+    playSound(keycode)
+  }
+
+  const playSound = (keycode) => {
+    const $audio = get(`audio[data-key="${keycode}"]`)
+    const $key = get(`div[data-key="${keycode}"]`)
+    if ($key && $audio) {
+      $key.classList.add('playing')
+      $audio.currentTime = 0
+      $audio.play()
+    }
+  }
+
+  const init = () => {
+    window.addEventListener('keydown', onKeyDown)
+    keys.forEach((key, index) => {
+      const audio = getAudioElement(index)
+      key.appendChild(audio)
+      key.addEventListener('transitionend', onTransitionEnd)
+      key.addEventListener('mousedown', onMouseDown)
+      key.dataset.key = drumSounds[index].key
+    })
+  }
+
+  init()
 })()
