6f689f9a8921ca8132b72f971da89c08dddbf198
sueWavy <sueboi0311@gmail.com>
2024-01-29 11:00:57 +0900

    프로스그래스바 만들기

--- case5_progress_bar/practice/script.js
+++ case5_progress_bar/practice/script.js
@@ -1,7 +1,33 @@
 ;(function () {
   'use strict'
 
+  let timerId
+
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  const throttle = (callback, time) => {
+    if (timerId) return
+    timerId = setTimeout(() => {
+      callback()
+      timerId = undefined
+    }, time)
+  }
+
+  const $progresBar = get('.progress-bar')
+
+  // 스크롤 길이 구하기
+  const onScroll = () => {
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight
+
+    const scrollTop = document.documentElement.scrollTop
+
+    const width = (scrollTop / height) * 100
+    $progresBar.style.width = width + '%'
+  }
+
+  window.addEventListener('scroll', () => throttle(onScroll, 50))
 })()
