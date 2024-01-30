20061891428519fff285a765705db849f23eb901
sueWavy <sueboi0311@gmail.com>
2024-01-30 17:42:47 +0900

    카카오 지도 API

--- case17_kakaomap/practice/script.js
+++ case17_kakaomap/practice/script.js
@@ -31,7 +31,76 @@
     return document.querySelector(target)
   }
 
-  const init = () => {}
+  const $map = get('#map')
+  const geoLocationButton = get('.geolocation_button')
+
+  const mapContainer = new kakao.maps.Map($map, {
+    center: new kakao.maps.LatLng(defaultPos.lat, defaultPos.lng),
+    level: 3,
+  })
+
+  const createMarkerImage = () => {
+    let markerImageSrc = 'assets/marker.png'
+    let imageSize = new kakao.maps.Size(30, 46)
+    return new kakao.maps.MarkerImage(markerImageSrc, imageSize)
+  }
+
+  const createMarker = (lat, lng) => {
+    const marker = new kakao.maps.Marker({
+      map: mapContainer,
+      position: new kakao.maps.LatLng(lat, lng),
+      image: createMarkerImage(),
+    })
+    return marker
+  }
+
+  const createShopElement = () => {
+    shops.map((shop) => {
+      const { lat, lng } = shop
+      const marker = createMarker(lat, lng)
+      const infowindow = new kakao.maps.InfoWindow({
+        content: `<div style="width:150px;text-align:center;padding:6px 2px;">
+                  <a href="https://place.map.kakao.com/${shop.id}" target="_blank">${shop.name}</a>
+                </div>`,
+      })
+      infowindow.open(mapContainer, marker)
+    })
+  }
+
+  const successGeolocation = (position) => {
+    const { latitude, longitude } = position.coords
+    mapContainer.setCenter(new kakao.maps.LatLng(latitude, longitude))
+    const marker = createMarker(latitude, longitude)
+    marker.setMap(mapContainer)
+  }
+
+  const errorGeolocation = (error) => {
+    if (error.code == 1) {
+      alert('위치 정보를 허용해주세요.')
+    } else if (error.code == 2) {
+      alert('사용할수 없는 위치 입니다.')
+    } else {
+      alert('오류가 발생했습니다.')
+    }
+  }
+
+  const getLocation = () => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        successGeolocation,
+        errorGeolocation
+      )
+    } else {
+      alert('지도 관련 api 를 불러올수 없습니다')
+    }
+  }
+
+  const init = () => {
+    geoLocationButton.addEventListener('click', () => {
+      getLocation()
+    })
+    createShopElement()
+  }
 
   init()
 })()
